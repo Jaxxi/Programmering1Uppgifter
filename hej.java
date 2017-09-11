51d668308e7060c01a8fb2b3a404d6087ac6526c
Jaxxi <te14.linus.olofsson@umea.nti.se>
2017-09-11 10:18:46 +0200

    Work
    Started working

---
+++ hej.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class hej {
+  public static void main (String[] arg) {
+  }
+}
---
+++ sammansattatext.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class sammansattatext{
+  public static void main (String[] arg) {
+      JOptionPane.showMessageDialog(null, "Hej! " + " Välkommen till kursen i javaprogrammering. "
+      + System.getProperty("user.name") + "!");
+  }
+}
